295674ca7170fc6c7543b234e68477d9f80a85ab
SantiagoToledo <santi.nt2@gmail.com>
2019-10-18 16:16:00 -0300

    Cree AccesoADatos

---
+++ tp2/AccesoADatos/AlumnoABM.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoADatos
+{
+    public class AlumnoABM
+    {
+        public void altaAlumno(Alumno al, Conexion con = null)
+        {
+            try
+            {
+                if (con == null)
+                {
+                    con = new Conexion();
+                }
+                string sql = @"Insert into Alumnos(Apellido,Nombre,Dni,Fnacimiento) values( @Apellido ",'" + al.Nombre + "','" + al.Dni + "','" + al.Fnacimiento + "')";
+                con.Conectar();
+                var cmd = new SqlCommand(sql, co.);
+                cmd.Parameters.AddWithValue("@Apellido", al.Apellido);
+                cmd.Parameters.AddWithValue("@Apellido", al.Apellido);
+                cmd.ExecuteNonQuery();
+                con.Desconectar();
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+    }
+}
---
+++ tp2/AccesoADatos/Conexion.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoADatos
+{
+    public class Conexion
+    {
+        SqlConnection cn;
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+
+        public Conexion()
+        {
+            try
+            {
+                cn = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Database=Taller2;Integrated Security=True");
+                //Corregir datos
+            }
+
+            catch (Exception ex)
+            {
+                //Agregar Log
+            }
+        }
+
+        public void Conectar()
+        {
+            cn.Open();
+        }
+        public void Desconectar()
+        {
+            cn.Close();
+        }
+
+
+        public void NonQuery(string sql)
+        {
+            try
+            {
+                cn.Open();
+                cmd = new SqlCommand(sql, cn);
+                cmd.ExecuteNonQuery();
+                cn.Close();
+            }
+            catch (Exception ex)
+            {
+
+                //asdsd
+            }
+        }
+        public SqlDataReader Reader(string sql)
+        {
+            try
+            {
+                cn.Open();
+                cmd = new SqlCommand(sql, cn);
+                dr = cmd.ExecuteReader();
+                cn.Close();
+            }
+            catch (Exception ex)
+            {
+                //.
+            }
+            return dr;
+        }
+
+
+
+    }
+}
+
